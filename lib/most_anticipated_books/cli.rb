fb89f0360a427ba1723086f65744526d9c290a92
Rhiannon <rhiannon.csw@gmail.com>
2018-02-08 20:39:59 -0600

    Added user input to call method, with pseudo-code

--- lib/most_anticipated_books/cli.rb
+++ lib/most_anticipated_books/cli.rb
@@ -1,7 +1,27 @@
 class MostAnticipatedBooks::CLI
 	def call
 		puts "Welcome! Let's explore the Most Anticipated Books for the first half of 2018."
-		menu
+		
+		input = nil
+		until ["1", "2", "3", "4", "5", "exit"].include?(input) do
+			menu
+			input = gets.downcase.strip
+		end
+
+		case input
+		when "1"
+			puts "list all books"
+		when "2"
+			puts "month"
+		when "3"
+			puts "list genres"
+		when "4"
+			puts "list authors"
+		when "5"
+			puts "list books in translation"
+		when "exit"
+			exit
+		end
 	end
 
 	def menu
@@ -11,6 +31,6 @@ class MostAnticipatedBooks::CLI
 		puts "3. Explore represented genres."
 		puts "4. Explore the books' authors."
 		puts "5. Explore books in translation."
-		puts "Please choose by number, 1-5."
+		puts "Please choose by number, 1-5. To exit, type 'exit.'"
 	end
 end
