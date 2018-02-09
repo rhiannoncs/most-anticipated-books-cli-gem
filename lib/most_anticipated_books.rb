730c17e93cbe749915e27ec4883a6f91ce6815b3
Rhiannon <rhiannon.csw@gmail.com>
2018-02-08 20:12:16 -0600

    Added require_relative to cli file

--- lib/most_anticipated_books.rb
+++ lib/most_anticipated_books.rb
@@ -1,4 +1,5 @@
-require "most_anticipated_books/version"
+require_relative "most_anticipated_books/version"
+require_relative "most_anticipated_books/cli"
 
 module MostAnticipatedBooks
 end
--- lib/most_anticipated_books/cli.rb
+++ lib/most_anticipated_books/cli.rb
@@ -1,5 +1,5 @@
 class MostAnticipatedBooks::CLI
 	def call
-		
+		puts "Test call."
 	end
 end
