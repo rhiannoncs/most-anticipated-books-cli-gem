7ad98cacb1f072882c95b088e295e5d477723104
Rhiannon <rhiannon.csw@gmail.com>
2018-02-09 14:01:42 -0600

    More work on finding selectors for links, titles and authors.

--- lib/most_anticipated_books/scraper.rb
+++ lib/most_anticipated_books/scraper.rb
@@ -3,9 +3,15 @@ class MostAnticipatedBooks::Scraper
 	def self.scrape_millions(url)
 		doc = Nokogiri::HTML(open(url))
 
-		amazon_link = doc.css("a")
+		amazon_link = doc.css("p > em:nth-child(2) a")
+		amazon_link_2 = doc.css("p > a:nth-child(2)")
+		amazon_link_3 = doc.css("p > i:nth-child(2) a")
 
-		puts amazon_link
+		author = doc.css("p > em + strong:nth-child(3)") #matches links and titles from amazon_link
+		author_2 = doc.css("p > a + strong:nth-child(3)") #misses Lynne Tillman, Clarice Lyspector, etc.
+		
+
+		puts author_2
 	end
 
 end
