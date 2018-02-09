a966994316927863534a2e93b674ef19634ba888
Rhiannon <rhiannon.csw@gmail.com>
2018-02-08 20:50:47 -0600

    Added nokogiri to gemfile

--- lib/most_anticipated_books.rb
+++ lib/most_anticipated_books.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'nokogiri'
+
 require_relative "most_anticipated_books/version"
 require_relative "most_anticipated_books/cli"
 require_relative "most_anticipated_books/scraper"
