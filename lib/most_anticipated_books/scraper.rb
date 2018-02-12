68ee7035755f07564aa624b2c009d2352de2d3fb
Rhiannon <rhiannon.csw@gmail.com>
2018-02-12 13:04:05 -0600

    Added author to Millions scraper.

--- lib/most_anticipated_books/scraper.rb
+++ lib/most_anticipated_books/scraper.rb
@@ -15,25 +15,28 @@ class MostAnticipatedBooks::Scraper
 			matches_2 = paragraph.css("a[href]") & amazon_link_2
 			matches_3 = paragraph.css("a[href]") & amazon_link_3
 			if matches.length > 0
-				book = MostAnticipatedBooks::Book.new(matches.text)
+				book = MostAnticipatedBooks::Book.new(matches.text.gsub(/\A[[:space:]]+|[[:space:]]+\z/, ''))
 				book.amazon_url = matches[0]["href"]
 				book.description = paragraph.text
+				book.author = paragraph.css("strong")[0].text.delete(":")
 				if book.description.include?("translated by")
 					book.translator = book.description[/#{start}(.*?)#{finish}/m, 1]
 				end
 			end
 			if matches_2.length > 0
-				book = MostAnticipatedBooks::Book.new(matches_2.text)
+				book = MostAnticipatedBooks::Book.new(matches_2.text.gsub(/\A[[:space:]]+|[[:space:]]+\z/, ''))
 				book.amazon_url = matches_2[0]["href"]
 				book.description = paragraph.text
+				book.author = paragraph.css("strong")[0].text.delete(":")
 				if book.description.include?("translated by")
 					book.translator = book.description[/#{start}(.*?)#{finish}/m, 1]
 				end
 			end
 			if matches_3.length > 0
-				book = MostAnticipatedBooks::Book.new(matches_3.text)
+				book = MostAnticipatedBooks::Book.new(matches_3.text.gsub(/\A[[:space:]]+|[[:space:]]+\z/, ''))
 				book.amazon_url = matches_3[0]["href"]
 				book.description = paragraph.text
+				book.author = paragraph.css("strong")[0].text.delete(":")
 				if book.description.include?("translated by")
 					book.translator = book.description[/#{start}(.*?)#{finish}/m, 1]
 				end
@@ -41,15 +44,15 @@ class MostAnticipatedBooks::Scraper
 		end
 		
 		MostAnticipatedBooks::Book.all.each do |book|
-			puts book.title + "/" + book.amazon_url + "/" + book.description
-			puts book.translator if book.translator
+			puts book.title + "/" + book.author if book.author
+			#puts book.translator if book.translator
 		end
 	end
 
 	def self.scrape_amazon(book)
 		doc = Nokogiri::HTML(open(book.amazon_url))
 
-		author = doc.xpath("//span[contains(@class, 'author notFaded')]/a[contains(@class, 'a-link-normal')]")
+		#author = doc.xpath("//span[contains(@class, 'author notFaded')]/a[contains(@class, 'a-link-normal')]")
 		#{}/*[descendant::span[@class="contribution"]]"
 		#site_title = doc.css("title").text.split(": ")
 		#author = site_title[site_title.index("Books") - 1]
@@ -57,6 +60,8 @@ class MostAnticipatedBooks::Scraper
 		#notFaded = doc.css(".author.notFaded a.a-link-normal")
 		#notFaded_text = notFaded.collect {|node| node.text}
 		#author = site_title & notFaded_text
+
+		author = doc.css(".author.notFaded a.a-link-normal")
 
 		puts author
 
