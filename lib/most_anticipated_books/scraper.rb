34a12544a6e0c337defa9889624dab11c925309d
Rhiannon <rhiannon.csw@gmail.com>
2018-02-13 21:30:14 -0600

    Updated Amazon scraper to handle cases of missing data.

--- lib/most_anticipated_books/scraper.rb
+++ lib/most_anticipated_books/scraper.rb
@@ -22,6 +22,7 @@ class MostAnticipatedBooks::Scraper
 				if book.description.include?("translated by")
 					book.translator = book.description[/#{start}(.*?)#{finish}/m, 1]
 				end
+				puts "#{book.title} made"
 			end
 			if matches_2.length > 0
 				book = MostAnticipatedBooks::Book.new(matches_2.text.gsub(/\A[[:space:]]+|[[:space:]]+\z/, ''))
@@ -31,6 +32,7 @@ class MostAnticipatedBooks::Scraper
 				if book.description.include?("translated by")
 					book.translator = book.description[/#{start}(.*?)#{finish}/m, 1]
 				end
+				puts "#{book.title} made"
 			end
 			if matches_3.length > 0
 				book = MostAnticipatedBooks::Book.new(matches_3.text.gsub(/\A[[:space:]]+|[[:space:]]+\z/, ''))
@@ -40,41 +42,62 @@ class MostAnticipatedBooks::Scraper
 				if book.description.include?("translated by")
 					book.translator = book.description[/#{start}(.*?)#{finish}/m, 1]
 				end
+				puts "#{book.title} made"
 			end
 		end
 		
-		MostAnticipatedBooks::Book.all.each do |book|
-			puts book.title
-			puts book.amazon_url
-		end
+		#MostAnticipatedBooks::Book.all.each do |book|
+		#	puts book.title
+		#	puts book.amazon_url
+		#end
 	end
 
 	def self.scrape_amazon(book)
 		doc = Nokogiri::HTML(open(book.amazon_url))
 
-		book.publication_date = doc.css("div#booksTitle > div.a-section.a-spacing-none > h1#title > span:nth-child(3)").text.delete("–").strip
-		book.genre = doc.css("ul.zg_hrsr > li.zg_hrsr_item > span.zg_hrsr_ladder > a:nth-child(2)")[0].text
-		
-		book.isbn = doc.at_css('li:contains("ISBN-10")').text.sub("ISBN-10: ", '')
+		publication_date = doc.css("div#booksTitle > div.a-section.a-spacing-none > h1#title > span:nth-child(3)").text
+		publication_date.length == 0 ? book.publication_date = "Unknown" : book.publication_date = publication_date.delete("–").strip
 
+		genre = doc.css("ul.zg_hrsr > li.zg_hrsr_item > span.zg_hrsr_ladder > a:nth-child(2)")[0]
+		genre.nil? ? book.genre = "Unknown" : book.genre = genre.text
+
+		isbn = doc.at_css('li:contains("ISBN-10")').text.sub("ISBN-10: ", '')
+		isbn.nil? ? book.isbn = "Unknown" : book.isbn = isbn
+		
 		start = "Publisher:[[:space:]]"
 		finish = Regexp.escape('(')
-		book.publisher = doc.at_css('li:contains("Publisher")').text[/#{start}(.*?)[[:space:]]#{finish}/m, 1]
+		publisher = doc.at_css('li:contains("Publisher")').text[/#{start}(.*?)[[:space:]]#{finish}/m, 1]
+		publisher ? book.publisher = publisher : book.publisher = "Unknown"
 		
+
 		book.subgenres = []
 		subgenres = doc.css("ul.zg_hrsr > li.zg_hrsr_item > span.zg_hrsr_ladder > a")
-		subgenres.each do |genre|
-			book.subgenres << genre.text unless genre.text == "Books" || book.subgenres.include?(genre.text) || book.genre == genre.text
+		
+		if subgenres.text.length > 0
+			subgenres.each do |genre|
+				book.subgenres << genre.text unless genre.text == "Books" || book.subgenres.include?(genre.text) || book.genre == genre.text
+			end
+		else 
+			book.subgenres << "Unknown"
 		end
 		final_subgenres = doc.css("ul.zg_hrsr > li.zg_hrsr_item > span.zg_hrsr_ladder > b > a")
-		final_subgenres.each do |genre|
-			book.subgenres << genre.text unless genre.text == "Books" || book.subgenres.include?(genre.text) || book.genre == genre.text
-		end
 		
-		puts "Publication Date: #{book.publication_date}"
-		puts "Publisher: #{book.publisher}, ISBN: #{book.isbn}"
-		puts "Genre: #{book.genre}"
-		puts "Subgenres: #{book.subgenres}"
+		if final_subgenres.text.length > 0
+			final_subgenres.each do |genre|
+				book.subgenres << genre.text unless genre.text == "Books" || book.subgenres.include?(genre.text) || book.genre == genre.text
+			end
+		else
+			book.subgenres << "Unknown" unless book.subgenres.include?("Unknown")
+		end
+		puts "#{book.title} updated"
+		#puts "Author: #{book.author}"
+		#puts "Date: #{book.publication_date}"
+		#puts "Genre: #{book.genre}"
+		#puts "ISBN: #{book.isbn}"
+		#puts "Publisher: #{book.publisher}"
+		#puts "Subgenres: #{book.subgenres}"
+
+		
 	end
 
 end
