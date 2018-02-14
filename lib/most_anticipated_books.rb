5ea24dc9a86011a6469d935410902a3dfcf54a1f
Rhiannon <rhiannon.csw@gmail.com>
2018-02-13 21:28:29 -0600

    Added dependencies for new class files

--- lib/most_anticipated_books.rb
+++ lib/most_anticipated_books.rb
@@ -5,6 +5,8 @@ require_relative "most_anticipated_books/version"
 require_relative "most_anticipated_books/cli"
 require_relative "most_anticipated_books/scraper"
 require_relative "most_anticipated_books/book"
+require_relative "most_anticipated_books/genre"
+require_relative "most_anticipated_books/author"
 
 module MostAnticipatedBooks
 end
