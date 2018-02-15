9020ff5788505eda34ff30f0df1d62ce509a82f4
Rhiannon <rhiannon.csw@gmail.com>
2018-02-15 10:26:31 -0600

    Abstracted part of millions scraper into description_author_translator method.

--- lib/most_anticipated_books/scraper.rb
+++ lib/most_anticipated_books/scraper.rb
@@ -7,9 +7,6 @@ class MostAnticipatedBooks::Scraper
 		amazon_link_2 = doc.css("p > a:nth-child(2)")
 		amazon_link_3 = doc.css("p > i:nth-child(2) a")
 
-		start = "translated by[[:space:]]"
-		finish = Regexp.escape(')')
-
 		doc.css("p").each do |paragraph|
 			matches = paragraph.css("a[href]") & amazon_link
 			matches_2 = paragraph.css("a[href]") & amazon_link_2
@@ -17,30 +14,30 @@ class MostAnticipatedBooks::Scraper
 			if matches.length > 0
 				book = MostAnticipatedBooks::Book.new(matches.text.gsub(/\A[[:space:]]+|[[:space:]]+\z/, ''))
 				book.amazon_url = matches[0]["href"]
-				book.description = paragraph.text
-				book.author = paragraph.css("strong")[0].text.delete(":").strip
-				if book.description.include?("translated by")
-					book.translator = book.description[/#{start}(.*?)#{finish}/m, 1]
-				end
+				description_author_translator(paragraph, book)
 			end
 			if matches_2.length > 0
 				book = MostAnticipatedBooks::Book.new(matches_2.text.gsub(/\A[[:space:]]+|[[:space:]]+\z/, ''))
 				book.amazon_url = matches_2[0]["href"]
-				book.description = paragraph.text
-				book.author = paragraph.css("strong")[0].text.delete(":").strip
-				if book.description.include?("translated by")
-					book.translator = book.description[/#{start}(.*?)#{finish}/m, 1]
-				end
+				description_author_translator(paragraph, book)
 			end
 			if matches_3.length > 0
 				book = MostAnticipatedBooks::Book.new(matches_3.text.gsub(/\A[[:space:]]+|[[:space:]]+\z/, ''))
 				book.amazon_url = matches_3[0]["href"]
-				book.description = paragraph.text
-				book.author = paragraph.css("strong")[0].text.delete(":").strip
-				if book.description.include?("translated by")
-					book.translator = book.description[/#{start}(.*?)#{finish}/m, 1]
-				end
+				description_author_translator(paragraph, book)
 			end
+		end
+		MostAnticipatedBooks::Book.all.each {|book| puts book.title, book.author, book.description}
+	end
+
+	def self.description_author_translator(paragraph, book)
+		start = "translated by[[:space:]]"
+		finish = Regexp.escape(')')
+
+		book.description = paragraph.text
+		book.author = paragraph.css("strong")[0].text.delete(":").strip
+		if book.description.include?("translated by")
+			book.translator = book.description[/#{start}(.*?)#{finish}/m, 1]
 		end
 	end
 
